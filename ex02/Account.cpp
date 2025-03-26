397c4d98d65fa9daa38dd4abd18f106c35698bd6
laiyaohao <laiyaohao96@gmail.com>
2025-03-26 12:15:18 +0800

    chore: save state

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -5,7 +5,28 @@ Account::Account(int initial_deposit)
   _amount = initial_deposit;
 }
 
-Account::displayAccountsInfos()
+int Account::getNbAccounts(void)
 {
+  return (Account::_nbAccounts);
+}
+
+int Account::getNbDeposits(void)
+{
+  return (Account::_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void)
+{
+  return (Account::_totalNbWithdrawals);
+}
+
+int Account::getTotalAmount(void)
+{
+  return (Account::_totalAmount);
+}
+
+void  Account::displayAccountsInfos()
+{
+  // display time, which idk how to do yet
   
 }
