88daa5723889189baddbdb34b7ed588e359ea8e4
laiyaohao <laiyaohao96@gmail.com>
2025-03-30 23:10:37 +0800

    Reapply "feat(ex02-account): finished ex02, need to go back to ex01 to fix the ctrl-d (eof) bug"
    This reverts commit f5d399e4b8836f4a4f57ad53fe675dbf7cb0f00f.

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -16,7 +16,9 @@ Account::Account(int initial_deposit)
   Account::_nbAccounts++;
   Account::_totalAmount += initial_deposit;
   Account::_displayTimestamp();
-  std::cout << "index:" << Account::_accountIndex << ";amount:" << Account::_amount << ";created" << std::endl;
+  std::cout << " index:" << Account::_accountIndex;
+  std::cout << ";amount:" << Account::_amount;
+  std::cout << ";created" << std::endl;
 }
 
 int Account::getNbAccounts(void)
@@ -59,8 +61,12 @@ void  Account::_displayTimestamp(void)
 
 void  Account::displayAccountsInfos()
 {
-  // display time, which idk how to do yet
   Account::_displayTimestamp();
+  std::cout << " accounts:" << Account::getNbAccounts();
+  std::cout << ";total:" << Account::getTotalAmount(); 
+  std::cout << ";deposits:" << Account::getNbDeposits();
+  std::cout << ";withdrawals:" << Account::getNbWithdrawals();
+  std::cout << std::endl;
 }
 
 int Account::checkAmount(void) const
@@ -70,26 +76,67 @@ int Account::checkAmount(void) const
 
 Account::~Account(void)
 {
-  //destrcutor
+  Account::_displayTimestamp();
+  std::cout << " index:" << Account::_accountIndex;
+  std::cout << ";amount:" << Account::_amount;
+  std::cout << ";closed" << std::endl;
 }
 
 void  Account::makeDeposit(int deposit)
 {
-  (void)deposit;
+  Account::_nbDeposits += 1;
+  Account::_totalNbDeposits += 1;
+  Account::_totalAmount += deposit;
+  Account::_displayTimestamp();
+  std::cout << " index:" << Account::_accountIndex;
+  std::cout << ";p_amount:" << Account::_amount;
+  std::cout << ";deposit:" << deposit;
+  Account::_amount += deposit;
+  std::cout << ";amount:" << Account::_amount;
+  std::cout << ";nb_deposits:"<< Account::_nbDeposits;
+  std::cout << std::endl;
 }
 
 bool  Account::makeWithdrawal(int withdrawal)
 {
   (void)withdrawal;
+  Account::_displayTimestamp();
+  std::cout << " index:" << Account::_accountIndex;
+  std::cout << ";p_amount:" << Account::_amount;
+  if (Account::_amount < withdrawal)
+  {
+    std::cout << ";withdrawal:refused" << std::endl;
+    return (false);
+  }
+  std::cout << ";withdrawal:" << withdrawal;
+  Account::_amount -= withdrawal;
+  Account::_totalAmount -= withdrawal;
+  std::cout << ";amount:" << Account::_amount;
+  Account::_nbWithdrawals += 1;
+  Account::_totalNbWithdrawals += 1;
+  std::cout << ";nb_withdrawals:" << Account::_nbWithdrawals;
+  std::cout << std::endl;
   return (true);
 }
 
 void  Account::displayStatus(void) const
 {
-
+  Account::_displayTimestamp();
+  std::cout << " index:" << Account::_accountIndex;
+  std::cout << ";amouunt:" << Account::_amount;
+  std::cout << ";deposits:" << Account::_nbDeposits;
+  std::cout << ";withdrawals:" << Account::_nbWithdrawals;
+  std::cout << std::endl;
 }
 
 Account::Account(void)
 {
-
+  Account::_amount = 0;
+  Account::_accountIndex = Account::_nbAccounts;
+  Account::_nbAccounts++;
+  Account::_totalAmount += 0;
+  Account::_displayTimestamp();
+  std::cout << " index:" << Account::_accountIndex;
+  std::cout << ";amount:" << Account::_amount;
+  std::cout << ";created" << std::endl;
 }
